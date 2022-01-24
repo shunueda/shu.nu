e0c73a0e2c0044ddaf98725849bc2eb831edf17f
Shun Ueda <62182668+shun-ueda@users.noreply.github.com>
2022-01-24 12:55:37 -0500

    fix(slider): fix slider

--- domains/root/src/components/navbar.tsx
+++ domains/root/src/components/navbar.tsx
@@ -1,36 +1,32 @@
-import { useEffect, useState } from 'react'
 import classes from './navbar.module.scss'
 
-export default function Navbar() {
-  const [hash, setHash] = useState('')
-  useEffect(() => {
-    window.onhashchange = () => {
-      setHash(window.location.hash.substring(1))
-    }
-  }, [])
+interface Props {
+  sections: string[]
+  hash: string
+}
+
+export default function Navbar({ sections, hash }: Props) {
   return (
     <>
       <div className={`${classes.root} row`}>
-        {['home', 'skills', 'experiences', 'projects', 'conctacts'].map(
-          (item, index) => {
-            return (
-              <div
-                key={item}
-                className={`${classes.box} col-md content content__item`}
+        {sections.map((item, index) => {
+          return (
+            <div
+              key={item}
+              className={`${classes.box} col-md content content__item`}
+            >
+              <a
+                className={`${classes.text} link link--kale`}
+                href={`/#${item}`}
+                style={{
+                  color: hash === item ? '#000000' : '#d3d3d3'
+                }}
               >
-                <a
-                  className={`${classes.text} link link--kale`}
-                  href={`/#${item}`}
-                  style={{
-                    color: hash === item ? '#000000' : '#d3d3d3'
-                  }}
-                >
-                  {item}
-                </a>
-              </div>
-            )
-          }
-        )}
+                {item}
+              </a>
+            </div>
+          )
+        })}
       </div>
     </>
   )
--- domains/root/src/pages/index.tsx
+++ domains/root/src/pages/index.tsx
@@ -1,22 +1,36 @@
-import Navbar from '~/src/components/navbar'
-import classes from '~/src/pages/index.module.scss'
+import Navbar from 'components/navbar'
+import classes from 'pages/index.module.scss'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const sections = ['home', 'skills', 'experiences', 'projects', 'contacts']
+  const [hash, setHash] = useState('')
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.location.hash = sections[0]
+    }
+    window.onhashchange = () => {
+      setHash(window.location.hash.substring(1))
+    }
+  }, [sections])
   return (
     <div className={`container ${classes.root}`}>
       <div className={classes.navbar}>
-        <Navbar />
+        <Navbar sections={sections} hash={hash} />
       </div>
-      <div className={classes.body}>
-        <section>
-          <div style={{ background: 'red' }} />
-        </section>
-        <section>
-          <div style={{ background: 'green' }} />
-        </section>
-        <section>
-          <div style={{ background: 'yellow' }} />
-        </section>
+      <div
+        className={classes.body}
+        style={{
+          marginLeft: `calc(calc(-1 * calc(100vw - 100%) / 2) - ${sections.indexOf(
+            hash
+          )}00vw)`
+        }}
+      >
+        {/*<section>*/}
+        {/*  <div style={{ background: 'red' }} className='container'>*/}
+        {/*    HOME*/}
+        {/*  </div>*/}
+        {/*</section>*/}
       </div>
     </div>
   )
