b71ae98a6af9ba3a63243554c6939f10817ebed7
Shun Ueda <62182668+shun-ueda@users.noreply.github.com>
2022-01-26 22:21:25 -0500

    fix(build): fix types

--- domains/root/src/types.tsx
+++ domains/root/src/types.tsx
@@ -1,16 +1,18 @@
+import { RTNode } from '@prismicio/types'
+
 export interface Page {
   title: string
   body: JSX.Element
 }
 
 export interface Experience {
-  company: string
-  description: string
-  duration: string
-  link?: string
-  location: string
-  position: string
-  thumbnail?: string
+  company: [] | [RTNode, ...RTNode[]] | null | undefined
+  description: [] | [RTNode, ...RTNode[]] | null | undefined
+  duration: [] | [RTNode, ...RTNode[]] | null | undefined
+  link?: [] | [RTNode, ...RTNode[]] | null | undefined
+  location: [] | [RTNode, ...RTNode[]] | null | undefined
+  position: [] | [RTNode, ...RTNode[]] | null | undefined
+  thumbnail?: [] | [RTNode, ...RTNode[]] | null | undefined
 }
 
 export interface BodyProps {
