b73e53f7f02953ea7fee9c7efef5bf543ccde289
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-25 01:21:07 -0700

    force dynamic

--- apps/website/app/api/profile/route.ts
+++ apps/website/app/api/profile/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import getBrowser from '../util/getBrowser'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
   const browser = await getBrowser()
   const page = await browser.newPage()
