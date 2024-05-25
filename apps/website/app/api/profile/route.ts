5421369875e2f6bd2f12920386df1d5c8ba26277
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-25 00:43:27 -0700

    change version

--- apps/website/app/api/profile/route.ts
+++ apps/website/app/api/profile/route.ts
@@ -7,7 +7,7 @@ async function getBrowser() {
     args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      `https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
     ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true
