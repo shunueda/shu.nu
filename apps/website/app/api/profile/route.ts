1dc41821c4b99d291f898a33bad328450624b8e8
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-25 01:00:28 -0700

    trial

--- apps/website/app/api/profile/route.ts
+++ apps/website/app/api/profile/route.ts
@@ -1,24 +1,18 @@
-import chromium from '@sparticuz/chromium-min'
 import { NextRequest, NextResponse } from 'next/server'
-import puppeteer from 'puppeteer-core'
+import puppeteer from 'puppeteer'
 
 async function getBrowser() {
-  return puppeteer.launch({
-    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-    ),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true
-  })
+  return puppeteer.launch()
 }
 
 export async function GET(_: NextRequest) {
   const browser = await getBrowser()
   const page = await browser.newPage()
   await page.goto('https://www.linkedin.com/in/shunueda/')
-  await page.setViewport(chromium.defaultViewport)
+  await page.setViewport({
+    height: 1080,
+    width: 1920
+  })
   await page.waitForSelector('main')
   const mainContent = await page.evaluate(() => {
     const mainElement = document.querySelector('main')
