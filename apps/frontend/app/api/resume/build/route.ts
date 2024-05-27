5ad8c742fa75f233f856f9e39c062cdb88f840f2
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-27 15:09:45 -0700

    fix action

--- apps/frontend/app/api/resume/build/route.ts
+++ apps/frontend/app/api/resume/build/route.ts
@@ -14,6 +14,6 @@ export async function POST(req: NextRequest) {
     })
     .returning('id')
     .executeTakeFirst()
-  await triggerWorkflow('test.yaml', { id: result.id.toString() })
+  await triggerWorkflow('actions.yaml', { id: result.id })
   return cors(req, NextResponse.json(result))
 }
