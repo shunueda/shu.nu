5bf4abcea1209f694994a59562573bcd6d790926
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-27 15:37:12 -0700

    test file creation

--- apps/actions/src/index.ts
+++ apps/actions/src/index.ts
@@ -1,5 +1,8 @@
 import { getInput, setOutput } from '@actions/core'
+import { writeFileSync } from 'fs'
 
 const id = parseInt(getInput('id'))
+
+writeFileSync('a.txt', 'Hello, world!')
 
 setOutput('data', id)
