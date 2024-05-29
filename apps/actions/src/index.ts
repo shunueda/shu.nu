6a752c38f0208263297d1369946114c4076f9f50
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-28 18:07:47 -0700

    fix script

--- apps/actions/src/index.ts
+++ apps/actions/src/index.ts
@@ -1,26 +1,23 @@
 import { getInput } from '@actions/core'
 import { put } from '@vercel/blob'
-import { readFile, writeFile } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 import { $, tempfile } from 'zx'
 import generateLatex from './generateLatex'
 
-const id = parseInt(getInput('id'))
+// get input
+const id = getInput('id')
 
-const res = await fetch(`https://shu.nu/api/resume/data/${id}`, {
-  method: 'GET'
-})
+// get data
+const res = await fetch(`https://shu.nu/api/resume/data/${id}`)
 const json = await res.json()
 
-const filename = tempfile()
+// build latex
 const latex = generateLatex(json)
-await writeFile(filename, latex)
+await $`latexmk -pdf ${tempfile(id, latex)}`
 
-const out = await $`latexmk -pdf ${filename}`
-console.log(out.message)
-
-const buffer = await readFile(`${filename}.pdf`)
-
-await put(id.toString(), buffer, {
+// upload artifact
+const buffer = await readFile(`${id}.pdf`)
+await put(id, buffer, {
   token: process.env.BLOB_READ_WRITE_TOKEN,
   access: 'public',
   contentType: 'application/pdf'
